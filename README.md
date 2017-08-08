55c83e092fa7004796059c666e88f1b1a8637fa8
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-09 00:22:40 +0300

    2 functions were updated

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The script receives user data and displays those who sent the tasks for review after 24:00.
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+python seek_dev_nighters.py
+
+{'username': 'RamusikValery', 'timezone': 'Europe/Moscow', 'timestamp': 1502144846.0}
+{'username': 'linafruscianta', 'timezone': 'Europe/Moscow', 'timestamp': 1502138596.087338}
+{'username': 'НогмановИльдар', 'timezone': 'Europe/Moscow', 'timestamp': 1502134551.691079}
+{'username': 'piterskikhsa', 'timezone': 'Europe/Moscow', 'timestamp': 1502134047.0}
+{'username': 'KhorinVitaly', 'timezone': 'Asia/Almaty', 'timestamp': 1502132987.0}
+{'username': 'KhorinVitaly', 'timezone': 'Asia/Almaty', 'timestamp': 1502132699.394946}
+{'username': 'KhorinVitaly', 'timezone': 'Asia/Almaty', 'timestamp': 1502131741.115038}
+
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,33 @@
+import requests
+from datetime import datetime
+from pytz import timezone
+
+
 def load_attempts():
     pages = 1
     for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+        params = {'page': '2'}
+        response = requests.get('https://devman.org/api/challenges/solution_attempts/', params)
+        if response.ok:
+            attempts = response.json()
+            for attempt in attempts['records']:
+                yield {
+                    'username': attempt['username'],
+                    'timestamp': attempt['timestamp'],
+                    'timezone': attempt['timezone'],
+                }
 
-def get_midnighters():
-    pass
+
+def get_midnighters(user_data):
+    tz = user_data['timezone']
+    if user_data['timestamp']:
+        server_time = datetime.fromtimestamp(user_data['timestamp'])
+        user_time = timezone(tz).fromutc(server_time)
+        if user_time.hour < 6:
+            return True
+
 
 if __name__ == '__main__':
-  pass
+    for user_data in load_attempts():
+        if get_midnighters(user_data):
+            print(user_data)
